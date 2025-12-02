b06989c01a73c5c6307bc1fbf9630bba99593515
Estifanos <estifanosbereket297@gmail.com>
2025-12-02 03:36:34 +0300

    feat: added new domain to cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const port = process.env.PUBLIC_API_PORT ?? 3000;
   app.enableCors({
 
-    origin: ['http://localhost:3000', 'http://localhost:3001', "https://d3-client.vercel.app", "https://d3.beete-nibab.com"],
+    origin: ['http://localhost:3000', 'http://localhost:3001', "https://d3-client.vercel.app", "https://d3.beete-nibab.com", "https://front.beete-nibab.com/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     // allowedHeaders: ['Content-Type', 'Authorization'],
